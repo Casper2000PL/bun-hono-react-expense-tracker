25a448daf45005a26543f637e4ad8d751cbe94a0
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-06-23 15:08:24 +0200

    5. Hono Zod Validator & Dynamic Path Params

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
 type Expense = {
@@ -24,15 +25,42 @@ const createPostSchema = z.object({
 
 export const expensesRoute = new Hono()
   .get("/", async (c) => {
-    return c.json({ expenses: [] });
+    return c.json({ expenses: fakeExpenses });
   })
-  .post("/", async (c) => {
-    const data = await c.req.json();
-    const expense = createPostSchema.parse(data);
+  .post("/", zValidator("json", createPostSchema), async (c) => {
+    const expense = await c.req.valid("json");
 
-    console.log(expense.amount);
+    fakeExpenses.push({
+      id: fakeExpenses.length + 1,
+      title: expense.title,
+      amount: expense.amount,
+    });
 
-    console.log({ expense });
+    c.status(201);
 
     return c.json(expense);
+  })
+  .get("/:id{[0-9]+}", async (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+
+    const expense = fakeExpenses.find((expense) => expense.id === id);
+
+    if (!expense) {
+      return c.notFound();
+    }
+
+    return c.json({ expense });
+  })
+  .delete("/:id{[0-9]+}", async (c) => {
+    const id = Number.parseInt(c.req.param("id"));
+
+    const index = fakeExpenses.findIndex((expense) => expense.id === id);
+
+    if (index === -1) {
+      return c.notFound();
+    }
+
+    const deletedExpense = fakeExpenses.splice(index, 1)[0];
+
+    return c.json({ expense: deletedExpense });
   });
