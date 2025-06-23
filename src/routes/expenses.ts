ce6936728fd93581325c444402bbb716c96b964c
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-06-23 14:07:19 +0200

    2. Adding Routes

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -1,9 +1,25 @@
 import { Hono } from "hono";
 
+type Expense = {
+  id: number;
+  title: string;
+  amount: number;
+};
+
+const fakeExpenses: Expense[] = [
+  { id: 1, title: "Office Supplies", amount: 100 },
+  { id: 2, title: "Travel Expenses", amount: 200 },
+  { id: 3, title: "Software Licenses", amount: 150 },
+];
+
 export const expensesRoute = new Hono()
-  .get("/", (c) => {
+  .get("/", async (c) => {
     return c.json({ expenses: [] });
   })
-  .post("/", (c) => {
-    return c.json({});
+  .post("/", async (c) => {
+    const expense = await c.req.json();
+
+    console.log({ expense });
+
+    return c.json(expense);
   });
