01afdf39eacf10008227fee659cf356068d931a9
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-06-23 14:17:13 +0200

    3. Zod HTTP Validation

--- src/routes/expenses.ts
+++ src/routes/expenses.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { z } from "zod";
 
 type Expense = {
   id: number;
@@ -12,12 +13,24 @@ const fakeExpenses: Expense[] = [
   { id: 3, title: "Software Licenses", amount: 150 },
 ];
 
+const createPostSchema = z.object({
+  title: z.string().min(3).max(100),
+  amount: z
+    .number()
+    .int()
+    .positive()
+    .min(0, "Amount must be a positive number"),
+});
+
 export const expensesRoute = new Hono()
   .get("/", async (c) => {
     return c.json({ expenses: [] });
   })
   .post("/", async (c) => {
-    const expense = await c.req.json();
+    const data = await c.req.json();
+    const expense = createPostSchema.parse(data);
+
+    console.log(expense.amount);
 
     console.log({ expense });
 
