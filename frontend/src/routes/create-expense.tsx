10b13f54f7d273b5324975e57ec9ee108e3b9166
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-06-25 00:02:57 +0200

    15. Tanstack Form

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,38 +1,96 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useForm } from "@tanstack/react-form";
+import { api } from "@/lib/api";
 
 export const Route = createFileRoute("/create-expense")({
   component: CreateExpense,
 });
 
 function CreateExpense() {
+  const navigate = useNavigate();
+  const form = useForm({
+    defaultValues: {
+      title: "",
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      const res = await api.expenses.$post({ json: value });
+
+      if (!res.ok) {
+        throw new Error("Failed to create expense");
+      }
+
+      navigate({ to: "/expenses" });
+    },
+  });
+
   return (
-    <div className="p-4 max-w-xs mx-auto">
-      <h2>Create Expense</h2>
-      <form className="flex flex-col gap-4 mt-5">
-        <Label htmlFor="title" className="block">
-          Title
-        </Label>
-        <Input
-          id="title"
-          type="text"
-          placeholder="Enter expense title"
-          className="w-fit"
+    <div className="p-4 w-fit mx-auto">
+      <h2 className="text-center">Create Expense</h2>
+      <form
+        className="flex flex-col gap-5 mt-5 w-fit"
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+      >
+        <form.Field
+          name="title"
+          children={(field) => (
+            <>
+              <Label htmlFor="title" className="block">
+                Title
+              </Label>
+              <Input
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(e.target.value)}
+                type="text"
+                placeholder="Enter expense title"
+                className="w-fit"
+              />
+            </>
+          )}
         />
-        <Label htmlFor="title" className="block mt-2">
-          Number
-        </Label>
-        <Input
-          id="amount"
-          type="text"
-          placeholder="Enter amount"
-          className="w-fit"
+
+        <form.Field
+          name="amount"
+          children={(field) => (
+            <>
+              <Label htmlFor="amount" className="block mt-2">
+                Amount
+              </Label>
+              <Input
+                id={field.name}
+                name={field.name}
+                value={field.state.value}
+                onBlur={field.handleBlur}
+                onChange={(e) => field.handleChange(Number(e.target.value))}
+                type="number"
+                placeholder="Enter amount"
+                className="w-fit"
+              />
+            </>
+          )}
         />
-        <Button type="submit" className="mt-4 w-fit cursor-pointer">
-          Create Expense
-        </Button>
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button
+              type="submit"
+              className="mt-4 w-fit cursor-pointer self-center"
+              disabled={!canSubmit}
+            >
+              {isSubmitting ? "..." : "Create Expense"}
+            </Button>
+          )}
+        />
       </form>
     </div>
   );
