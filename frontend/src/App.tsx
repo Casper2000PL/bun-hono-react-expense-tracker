086d2df7fed03c9da6e6b178de70c166d6f1b88a
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-06-24 17:26:27 +0200

    10. Hono RPC

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-import { Button } from "./components/ui/button";
 import {
   Card,
-  CardAction,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useEffect, useState } from "react";
+import "./App.css";
+import { api } from "./lib/api";
 
 function App() {
   const [totalSpent, setTotalSpent] = useState(0);
 
   useEffect(() => {
     async function fetchTotalSpent() {
-      const res = await fetch("/api/expenses/total-spent");
+      const res = await api.expenses["total-spent"].$get();
       const data = await res.json();
       setTotalSpent(data.total);
     }
