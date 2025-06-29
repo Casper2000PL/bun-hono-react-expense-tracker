11a15c9d2824664488136885fee336e9f89eb7b2
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-06-29 23:33:28 +0200

    23. UI Update

--- frontend/src/routes/_authenticated.tsx
+++ frontend/src/routes/_authenticated.tsx
@@ -1,11 +1,17 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 import { userQueryOptions } from "@/lib/api";
+import { Button } from "@/components/ui/button";
 
 const Login = () => {
   return (
-    <div>
-      <p>You have to login</p>
-      <a href="/api/login">LOGIN</a>
+    <div className="flex flex-col gap-y-2 items-center">
+      <p>You have to login or register</p>
+      <Button asChild>
+        <a href="/api/login">Login!</a>
+      </Button>
+      <Button asChild>
+        <a href="/api/register">Register!</a>
+      </Button>
     </div>
   );
 };
@@ -19,14 +25,16 @@ const Component = () => {
   return <Outlet />;
 };
 
+// src/routes/_authenticated.tsx
 export const Route = createFileRoute("/_authenticated")({
   beforeLoad: async ({ context }) => {
     const queryClient = context.queryClient;
+
     try {
       const data = await queryClient.fetchQuery(userQueryOptions);
       return data;
-    } catch (error) {
-      return { user: null }; // Return null if the user is not authenticated
+    } catch (e) {
+      return { user: null };
     }
   },
   component: Component,
